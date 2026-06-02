6a02491b70804fe8f0cd051a1e15a4943fdff2b0
Henn Sarv <henn@sarv.ee>
2026-06-02 21:30:48 +0300

    esimene kaart

---
+++ henn-first-card.js
@@ -0,0 +1,19 @@
+class HennFirstCard extends HTMLElement {
+
+    setConfig(config) {
+        this.innerHTML = `
+            <ha-card header="Hennu esimene kaart">
+                <div style="padding:16px">
+                    <p>Tere Henn!</p>
+                    <p>Minu esimene Home Assistant kaart töötab.</p>
+                </div>
+            </ha-card>
+        `;
+    }
+
+    getCardSize() {
+        return 2;
+    }
+}
+
+customElements.define("henn-first-card", HennFirstCard);
