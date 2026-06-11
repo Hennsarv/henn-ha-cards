dee72353648dbd6fa24a39e89c2fed66247177bd
Henn Sarv <henn@sarv.ee>
2026-06-11 18:50:19 +0300

    proovime teisipidi vahelduv baseline

--- dist/henn-ha-cards.js
+++ dist/henn-ha-cards.js
@@ -1495,7 +1495,7 @@ class HennStonehengeCard extends HTMLElement {
         let result = "";
 
         if (fillShow) {
-            const baseRadius = upper;
+            const baseRadius = c.anchor === "upper" ? upper : lower;
             const fillPoints = [];
 
             for (let i = 0; i < buckets.length; i++) {
