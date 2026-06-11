7e524c3ef98f70bc9127a1b6a5471f207c575e62
Henn Sarv <henn@sarv.ee>
2026-06-11 16:51:06 +0300

    veelkorra tagasi - ei tee eraldi merget

--- dist/henn-ha-cards.js
+++ dist/henn-ha-cards.js
@@ -1192,10 +1192,12 @@ class HennStonehengeCard extends HTMLElement {
                 color: "white",
                 margin: 12
             },
-            ...config,
+            lower: { stroke: 1, color: "white", radius: 30 },
+            upper: { stroke: 1, color: "white", radius: 90 },
+            ...config
 
-            lower: { stroke: 1, color: "white", radius: 30, ...config.lower },
-            upper: { stroke: 1, color: "white", radius: 90, ...config.upper }
+            // lower: { stroke: 1, color: "white", radius: 30, ...config.lower },
+            // upper: { stroke: 1, color: "white", radius: 90, ...config.upper }
 
         };
     }
