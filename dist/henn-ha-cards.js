52be269b5039f2e19d570197f3acc286efdb052c
Henn Sarv <henn@sarv.ee>
2026-06-13 21:37:21 +0300

    proovime otse width = fontsize * 2

--- dist/henn-ha-cards.js
+++ dist/henn-ha-cards.js
@@ -1225,11 +1225,10 @@ class HennStonehengeCard extends HTMLElement {
         };
 
         var fontSize = this.config.ticks.font_size ?? this.config.ticks.font?.size ?? 5;
-        var width = this.config.ticks.width ?? 0;
+        var width = this.config.ticks.width ?? (fontSize * 2);
         var radius = this.config.ticks.radius;
-        if (width === 0) width = fontSize * 2;
         this.config.ticks.width = width;
-        //console.info("width ja fontsize", width, fontSize);
+        console.info("width ja fontsize", width, fontSize);
         this.config.ticks.inner.radius ??= (radius - width / 2);
         this.config.ticks.outer.radius ??= (radius + width / 2);
         this.config.ticks.minor.radius ??= radius;
