bb906dbb753abeb4730e20e2190931bab505fa93
Henn Sarv <henn@sarv.ee>
2026-06-29 13:08:41 +0300

    doubleslider ei hakka tööle?

--- dist/henn-ha-cards.js
+++ dist/henn-ha-cards.js
@@ -2895,7 +2895,7 @@ class HennStonehengeCardEditor extends HTMLElement {
         const maxOpacity = defaults.gradient?.max_opacity ?? 1
 
         const minOpacityNumber = hennNumberInput(this, "gradient.min_opacity", minOpacity, 0,
-            { min: 0, max: 0.5, step: 0.5, style: { display: "none" } });
+            { min: 0, max: 0.5, step: 0.05, style: { display: "none" } });
 
         const doubleSliderOpacity = hennDoubleSliderCore(minOpacity, maxOpacity, 0, 1, 0.05, {
             minGap: 10,
@@ -2910,7 +2910,7 @@ class HennStonehengeCardEditor extends HTMLElement {
         });
 
         const maxOpacityNumber = hennNumberInput(this, "gradient.max_opacity", maxOpacity, 1,
-            { min: 0.5, max: 1, step: 0.5, style: { display: "none" } });
+            { min: 0.5, max: 1, step: 0.05, style: { display: "none" } });
 
 
 
