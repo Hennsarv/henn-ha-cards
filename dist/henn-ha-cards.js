7adf936950ad645176347b1b1f5ad78b1c30b427
Henn Sarv <henn@sarv.ee>
2026-06-29 19:59:14 +0300

    vist oli näpukas

--- dist/henn-ha-cards.js
+++ dist/henn-ha-cards.js
@@ -2919,7 +2919,7 @@ class HennStonehengeCardEditor extends HTMLElement {
 
 
         gradientTable2.appendChild(hennMultiBox([minOpacityNumber, doubleSliderOpacity, maxOpacityNumber]));
-        gradientTable2.appendChild(hennSubTitle(`Gradient opacity ${minOpacity}-${maxOpacity}`).addStyle({aligneContent: "center"}));
+        gradientTable2.appendChild(hennSubTitle(`Gradient opacity ${minOpacity}-${maxOpacity}`).addStyle({alignContent: "center"}));
 
 
         body.appendChild(gradientTable);
